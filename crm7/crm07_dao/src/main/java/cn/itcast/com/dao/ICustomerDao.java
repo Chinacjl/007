900cfef07d666a8ad11535c45a86fc16ba4a440e
cjl <2447625845@qq.com>
2019-03-20 01:19:23 +0800

    007

---
+++ crm7/crm07_dao/src/main/java/cn/itcast/com/dao/ICustomerDao.java
@@ -0,0 +1,5 @@
+package cn.itcast.com.dao;
+
+public interface ICustomerDao {
+
+}
---
+++ crm7/crm07_dao/src/main/java/cn/itcast/com/dao/impl/CustomerDao.java
@@ -0,0 +1,7 @@
+package cn.itcast.com.dao.impl;
+
+import cn.itcast.com.dao.ICustomerDao;
+
+public class CustomerDao implements ICustomerDao {
+
+}
